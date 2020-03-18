e5d98c4204fc2344ff2b3c111d09313b9e74c34e
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:11:53 +0000

    QuoteBox added and css created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav/Nav'
+import QuoteBox from './QuoteBox/QuoteBox'
 
 function App() {
   return (
     <div className="App">
       <Nav / >
         <main className="container">
+          <QuoteBox />
 
-          
         </main>
     </div>
   );
