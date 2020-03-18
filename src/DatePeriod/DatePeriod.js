87ded554b6c86b446a1f2e33a404ace49c21109e
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:20:21 +0000

    updated

---
+++ src/DatePeriod/DatePeriod.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DatePeriod() {
+    return (
+
+        <div className="row mt-4">
+            <div className="col">
+                <h1 className="PeriodTitles">TODAY</h1>
+            </div>
+        </div>
+    );
+}
+
+export default DatePeriod;
