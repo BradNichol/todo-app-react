373291195e55402a8537c03316aba62be849871f
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:29:30 +0000

    Task JSX added

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Task() {
     return (
-
+        <div className="row mt-4">
+            <div className="col">
+                <div className="card shadow-sm todoCard">
+                    <div className="card-body">
+                        To do 1
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
