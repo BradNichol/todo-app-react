76929e1502cae9ef549af649579f2eac1fe0df7d
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:59:42 +0000

    further Tasks added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,12 @@ function App() {
           <QuoteBox />
           <DatePeriod title="TODAY" />
           <Task text="Complete proposal" />
+          <Task text="Call the buyer" />
+          <Task text="Organise the shipment" />
+          <Task text="Order stock" />
           <DatePeriod title="TOMORROW" />
+          <Task text="Meeting at 9am" />
+          <Task text="send notes to team" />
           <AddButton />
 
         </main>
