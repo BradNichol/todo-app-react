d810e2786f28fc36d48b010ccd44da1cb76870fd
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:06:54 +0000

    QuoteBox component created

---
+++ src/QuoteBox/QuoteBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function QuoteBox() {
+    return (
+        <div className="row">
+            <div className="col mt-4">
+                <div className="quoteBox">
+                    <p>"It does not matter how slowly you go as long as you do not stop."</p>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default QuoteBox;
