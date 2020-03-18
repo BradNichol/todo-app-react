731d4f83363809be81a65b52249f5b364cfc2d54
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 14:06:28 +0000

    Updates

---
+++ precache-manifest.d27ecb9831ffd74978f6b3fc1a9f4397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91281d258f79e8ae493e5598a2ebe69c",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "9195a0cb85fc4912a2f5",
+    "url": "/todo-app-react/static/css/main.da73ffc2.chunk.css"
+  },
+  {
+    "revision": "e53e790579d2e6ade80a",
+    "url": "/todo-app-react/static/js/2.baf810e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-app-react/static/js/2.baf810e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9195a0cb85fc4912a2f5",
+    "url": "/todo-app-react/static/js/main.0fda65a5.chunk.js"
+  },
+  {
+    "revision": "71d0c8c65fd3b23b2640",
+    "url": "/todo-app-react/static/js/runtime-main.665a34b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
