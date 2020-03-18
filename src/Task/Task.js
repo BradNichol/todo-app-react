2b386cb9df72d1a58aad3a3ec57b8c4216f42225
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-18 12:30:08 +0000

    task css name updated

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -4,7 +4,7 @@ function Task() {
     return (
         <div className="row mt-4">
             <div className="col">
-                <div className="card shadow-sm todoCard">
+                <div className="card shadow-sm task">
                     <div className="card-body">
                         To do 1
                     </div>
