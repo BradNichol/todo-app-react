886e3af6a9864c6cab14381e72d1174c40ac62c2
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-25 14:32:08 +0000

    state added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav/Nav'
@@ -7,17 +7,33 @@ import DatePeriod from './DatePeriod/DatePeriod'
 import Task from './Task/Task'
 import AddButton from './Task/AddButton'
 
+
 function App() {
+
+  const [tasks, SetTasks] = useState([
+    {
+      task: "Organise a meeting",
+      type: "work",
+      dueDate: "2020-03-04",
+      completed: false,
+      id: 1
+    }
+  
+  ]);
+
   return (
     <div className="App">
       <Nav / >
         <main className="container">
           <QuoteBox />
           <DatePeriod title="TODAY" />
-          <Task text="Complete proposal" />
-          <Task text="Call the buyer" />
-          <Task text="Organise the shipment" />
-          <Task text="Order stock" />
+          {tasks.map(task => {
+            return (
+              <Task
+                text={task.task}
+              />
+            );
+          })}
           <DatePeriod title="TOMORROW" />
           <Task text="Meeting at 9am" />
           <Task text="send notes to team" />
