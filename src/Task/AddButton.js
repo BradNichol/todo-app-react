f1f388529cfe7bb5c7943f4f3c4523b69d75f292
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-25 23:39:01 +0000

    used useStyles to for FAB button, to float

--- src/Task/AddButton.js
+++ src/Task/AddButton.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Task.css';
+import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
 
 
 function AddButton() {
+
+    const useStyles = makeStyles((button) => ({
+        fab: {
+          position: 'fixed',
+          bottom: button.spacing(2),
+          right: button.spacing(2),
+        },
+      }));
+    
+    const classes = useStyles();
+    
     return(
         
-        <Fab position="left-top" size="large" color="primary" aria-label="add">
+        <Fab size="large" color="primary" aria-label="add" className={classes.fab}>
             <AddIcon />
         </Fab>
         
