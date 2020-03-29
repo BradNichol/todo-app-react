d5fd5a65ca88cb40e949d92c9ab734710c131186
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-29 20:33:44 +0100

    function created for completed

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Task.css';
 
 function Task(props) {
+
+    const handleCompletedStrikethrough = () => {
+        props.markCompleteFunc(props.id);
+    }
+
     return (
         <div className="row ">
             <div className="col">
@@ -9,7 +14,7 @@ function Task(props) {
                     <div className="card-body">
                         <div className="row">
                             <div className="col-2">
-                                <input className="form-check-input" type="checkbox" value="" />
+                                <input className="form-check-input" type="checkbox" value="" onClick={handleCompletedStrikethrough} />
                             </div>
                             <div className="col-10">
                                 {props.text}
