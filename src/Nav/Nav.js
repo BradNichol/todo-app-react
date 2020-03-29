d83ed5bf6fdb4b8668c94caefffa450110ff06a0
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-29 23:45:44 +0100

    material-ui imports added for nav

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,10 +1,16 @@
 import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
 
 function Nav() {
     return (
 
         <header className="container-fluid">
             <nav>
+
+
                 
             </nav>
         </header>
