19f50286d553da926ca6830637f5b472a0eedbe1
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-29 22:35:21 +0100

    test modal added

--- src/AddModal/AddModel.js
+++ src/AddModal/AddModel.js
@@ -3,6 +3,10 @@ import Modal from '@material-ui/core/Modal';
 
 const AddModal = () => {
     return (
+        <div>
+            <button type="button" onClick={handleOpen}>Click</button>
+            <Modal open={open} onClose={handleClose}>Test Modal</Modal>
+        </div>
 
     );
 }
