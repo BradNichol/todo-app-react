5e27d64ea99de72d9070af84703f5dbdfb9eeb2f
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-30 20:46:34 +0100

    notification added with badge (to display no. of tasks)

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,24 +4,31 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/core/Menu";
 import { makeStyles } from "@material-ui/core/styles";
+import Badge from "@material-ui/core/Badge";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Nav() {
   const useStyles = makeStyles({
-      root: {
-          backgroundColor: '#2b71c2',
-      }
+    root: {
+      backgroundColor: "#2b71c2"
+    }
   });
 
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <header className="container-fluid">
       <nav>
-        <AppBar position="fixed" className={classes.root} >
-          <Toolbar> 
+        <AppBar position="fixed" className={classes.root}>
+          <Toolbar>
             <IconButton edge="start" aria-label="menu">
               <MenuIcon />
             </IconButton>
             GET SH*T DONE
+            <IconButton>
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
           </Toolbar>
         </AppBar>
         <Toolbar />
