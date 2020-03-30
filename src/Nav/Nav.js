979db8a07ffd8f42542d657459d97af097da3b88
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-30 18:58:39 +0100

    code formatting

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,30 +1,25 @@
 import React from "react";
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/Menu';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/core/Menu";
 
 function Nav() {
-    return (
-
-        <header className="container-fluid">
-            <nav>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton edge="start" aria-label="menu">
-                            <MenuIcon />
-                        </IconButton>
-                        GET SH*T DONE
-                    </Toolbar>
-                </AppBar>
-                <Toolbar />
-                
-
-
-                
-            </nav>
-        </header>
-    );
+  return (
+    <header className="container-fluid">
+      <nav>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton edge="start" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            GET SH*T DONE
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+      </nav>
+    </header>
+  );
 }
 
 export default Nav;
