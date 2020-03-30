13821e93f1fe3d19eae701aaac4ea80bb2c67560
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-30 21:36:46 +0100

    prettier formatting

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import './Task.css';
+import React from "react";
+import "./Task.css";
 
 function Task(props) {
+  const handleCompletedStrikethrough = () => {
+    props.markCompleteFunc(props.id);
+  };
 
-    const handleCompletedStrikethrough = () => {
-        props.markCompleteFunc(props.id);
-    }
-
-    return (
-        <div className="row ">
-            <div className="col">
-                <div className="card shadow-sm task">
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-2">
-                                <input className="form-check-input" type="checkbox" value="" onClick={handleCompletedStrikethrough} />
-                            </div>
-                            <div className="col-10">
-                                {props.completed === false ? props.text : <del>{props.text}</del>}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="row ">
+      <div className="col">
+        <div className="card shadow-sm task">
+          <div className="card-body">
+            <div className="row">
+              <div className="col-2">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  onClick={handleCompletedStrikethrough}
+                />
+              </div>
+              <div className="col-10">
+                {props.completed === false ? (
+                  props.text
+                ) : (
+                  <del>{props.text}</del>
+                )}
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Task;
