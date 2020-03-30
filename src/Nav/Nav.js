c2f6e3fd6e0783b6037218412f8b268f0643a281
Bradley Nichol <bradleynichol@live.co.uk>
2020-03-30 20:26:29 +0100

    styles added to Nav AppBar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,13 +3,21 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/core/Menu";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Nav() {
+  const useStyles = makeStyles({
+      root: {
+          backgroundColor: '#2b71c2',
+      }
+  });
+
+  const classes = useStyles()
   return (
     <header className="container-fluid">
       <nav>
-        <AppBar position="fixed">
-          <Toolbar>
+        <AppBar position="fixed" className={classes.root} >
+          <Toolbar> 
             <IconButton edge="start" aria-label="menu">
               <MenuIcon />
             </IconButton>
