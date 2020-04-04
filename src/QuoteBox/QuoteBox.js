c16292cb325183ae18736f20e8f6737bd8fefed0
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-04 13:11:07 +0100

    prettier formatting

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -2,28 +2,24 @@ import React from "react";
 import "./QuoteBox.css";
 
 function QuoteBox() {
-  
   // fetch API is used to get data from URL resources
-  fetch('https://quotes.rest/qod?language=en')
-  //.then returns a promise and data can be chained to next .then. 
-  .then((response) => {
-    //returns data in JSON format
-    return response.json();
-  })
-  .then((data) => {
-    const obj = data.contents.quotes[0]
-    document.getElementById('quote').innerText = `"${obj.quote}"`
-    document.getElementById('author').innerText = ` - ${obj.author}`
-    
-  });
-
-  
+  fetch("https://quotes.rest/qod?language=en")
+    //.then returns a promise and data can be chained to next .then.
+    .then(response => {
+      //returns data in JSON format
+      return response.json();
+    })
+    .then(data => {
+      const obj = data.contents.quotes[0];
+      document.getElementById("quote").innerText = `"${obj.quote}"`;
+      document.getElementById("author").innerText = ` - ${obj.author}`;
+    });
 
   return (
     <div className="row">
       <div className="col mt-4">
         <div className="quoteBox">
-          <p id ="quote"></p>
+          <p id="quote"></p>
           <p id="author"></p>
         </div>
       </div>
