1376e3a974e1c5dad7b141b5927b8c3a4ab7bf0f
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-04 13:07:13 +0100

    Accessed DOM directly to add quote

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -3,29 +3,26 @@ import "./QuoteBox.css";
 
 function QuoteBox() {
   
-  const getQuote = () => {
+  // fetch API is used to get data from URL resources
+  fetch('https://quotes.rest/qod?language=en')
+  //.then returns a promise and data can be chained to next .then. 
+  .then((response) => {
+    //returns data in JSON format
+    return response.json();
+  })
+  .then((data) => {
+    const obj = data.contents.quotes[0]
+    document.getElementById('quote').innerText = obj.quote
+    
+  });
 
-    // fetch API is used to get data from URL resources
-    fetch('https://quotes.rest/qod?language=en')
-    //.then returns a promise and data can be chained to next .then. 
-    .then((response) => {
-      //returns data in JSON format
-      return response.json();
-    })
-    .then((data) => {
-      return data.contents.quotes[0].quote;
-    });
-  }
-  
   
 
   return (
     <div className="row">
       <div className="col mt-4">
         <div className="quoteBox">
-          <p>
-            "It does not matter how slowly you go as long as you do not stop."
-          </p>
+          <p id ="quote"></p>
         </div>
       </div>
     </div>
