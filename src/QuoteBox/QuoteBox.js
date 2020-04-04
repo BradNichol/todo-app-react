0bab4fcf33574f90ce95a5e026bbdf105eabb33e
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-04 11:35:28 +0100

    added Fetch API to get daily quote

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -2,6 +2,19 @@ import React from "react";
 import "./QuoteBox.css";
 
 function QuoteBox() {
+  
+  // fetch API is used to get data from URL resources
+  fetch('https://quotes.rest/qod?language=en')
+  //.then returns a promise and data can be chained to next .then. 
+  .then((response) => {
+    //returns data in JSON format
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.contents.quotes[0].quote);
+  });
+  
+
   return (
     <div className="row">
       <div className="col mt-4">
