7ca57749a860301835b56f2be791541e1f03d238
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-05 17:42:47 +0100

    prettier formatting applied

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,38 +1,38 @@
 import React from "react";
 import "./Task.css";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 function Task(props) {
   const handleCompletedStrikethrough = () => {
     props.markCompleteFunc(props.id);
   };
 
-  const useStyles = makeStyles ({
+  const useStyles = makeStyles({
     root: {
-      display: 'flex',
-      minWidth: 'auto',
-      backgroundColor: '#224982',
-      marginTop: 5
+      display: "flex",
+      minWidth: "auto",
+      backgroundColor: "#224982",
+      marginTop: 5,
     },
     content: {
-      margin: 'auto',
-      color: 'white'
-    }
-  })
+      margin: "auto",
+      color: "white",
+    },
+  });
   const classes = useStyles();
   return (
     <div>
       <Card className={classes.root} boxShadow={3}>
         <CardContent className={classes.content}>
-        <input
-          className="form-check-input"
-          type="checkbox"
-          value=""
-          onClick={handleCompletedStrikethrough}
-        />
-        {props.completed === false ? (props.text) : ( <del>{props.text}</del>)}
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            onClick={handleCompletedStrikethrough}
+          />
+          {props.completed === false ? props.text : <del>{props.text}</del>}
         </CardContent>
       </Card>
     </div>
