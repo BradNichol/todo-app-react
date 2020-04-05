9cd30f9b9afe0b8c4c6a116aa7a52f7438d4745c
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-05 17:40:05 +0100

    changed from Bootstrap to Material-ui cards

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,32 +1,39 @@
 import React from "react";
 import "./Task.css";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 function Task(props) {
   const handleCompletedStrikethrough = () => {
     props.markCompleteFunc(props.id);
   };
 
+  const useStyles = makeStyles ({
+    root: {
+      display: 'flex',
+      minWidth: 'auto',
+      backgroundColor: '#224982',
+      marginTop: 5
+    },
+    content: {
+      margin: 'auto'
+    }
+  })
+  const classes = useStyles();
   return (
-    <div className="row ">
-      <div className="col">
-        <div className="card shadow-sm task">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-2">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  value=""
-                  onClick={handleCompletedStrikethrough}
-                />
-              </div>
-              <div className="col-10">
-                {props.completed === false ? (props.text) : ( <del>{props.text}</del>)}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Card className={classes.root} boxShadow={3}>
+        <CardContent className={classes.content}>
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value=""
+          onClick={handleCompletedStrikethrough}
+        />
+        {props.completed === false ? (props.text) : ( <del>{props.text}</del>)}
+        </CardContent>
+      </Card>
     </div>
   );
 }
