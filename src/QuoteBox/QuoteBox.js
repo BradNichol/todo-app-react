b75c935b6a25a1e816793a8817c1836e7f4184d6
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-09 11:57:25 +0100

    styles added to panel

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -4,6 +4,8 @@ import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { makeStyles } from "@material-ui/core/styles";
+
 
 function QuoteBox() {
   // fetch API is used to get data from URL resources
@@ -19,8 +21,18 @@ function QuoteBox() {
       document.getElementById("author").innerText = ` - ${obj.author}`;
     });
 
+  
+  const useStyles = makeStyles(() => ({
+    expansionpanel: {
+      backgroundColor: '#2071C2'
+    }
+  
+  }))
+
+  const classes = useStyles();
+
   return (
-    <ExpansionPanel>
+    <ExpansionPanel className={classes.expansionpanel}>
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
         Quote of the day
       </ExpansionPanelSummary>
