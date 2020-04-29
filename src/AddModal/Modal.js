3dc391020df365f0d2e7689391089c5641538fda
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-29 22:24:44 +0100

    seperated Form into onwn component

--- src/AddModal/Modal.js
+++ src/AddModal/Modal.js
@@ -3,17 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
-import TextField from "@material-ui/core/TextField";
-import DateFnsUtils from "@date-io/date-fns";
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from "@material-ui/pickers";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
-import Button from "@material-ui/core/Button";
-import FormGroup from "@material-ui/core/FormGroup";
+import Form from "./Form";
+
 
 const AddModal = (props) => {
   const useStyles = makeStyles((theme) => ({
@@ -35,12 +26,6 @@ const AddModal = (props) => {
       bottom: theme.spacing(2),
       right: theme.spacing(2),
     },
-    modalForm: {
-      padding: 10,
-    },
-    modalButton: {
-      marginTop: 40,
-    },
   }));
 
   // state to control open / close of modal
@@ -48,25 +33,6 @@ const AddModal = (props) => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  // state to control task input field
-  const [taskText, setTaskText] = useState("");
-  const handleTaskTextChange = (event) => setTaskText(event.target.value);
-
-  // state to control date picker change
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const handleDateChange = (date) => setSelectedDate(date);
-
-  // state to control type select change
-  const [type, setType] = useState("");
-  const handleTypeChange = (event) => setType(event.target.value);
-
-  const handleAddTask = () => {
-    props.addNewTaskFunc(taskText, type, selectedDate);
-    setTaskText("");
-    setType();
-    setSelectedDate(new Date());
-    setOpen(false);
-  };
 
   const classes = useStyles("");
 
@@ -85,42 +51,7 @@ const AddModal = (props) => {
       <Modal open={open} className={classes.modal} onClose={handleClose}>
         <div className={classes.paper}>
           <h3>Add A Task</h3>
-          <form>
-            <FormGroup className={classes.modalForm}>
-              <TextField
-                id="task"
-                label="Task"
-                value={taskText}
-                onChange={handleTaskTextChange}
-              />
-              <InputLabel id="task-type">Task Type</InputLabel>
-              <Select
-                labelId="task-type"
-                id="task-type-select"
-                value={type}
-                onChange={handleTypeChange}
-              >
-                <MenuItem value="work">Work</MenuItem>
-                <MenuItem value="personal">Personal</MenuItem>
-              </Select>
-
-              <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker
-                  value={selectedDate}
-                  label="Due Date"
-                  onChange={handleDateChange}
-                />
-              </MuiPickersUtilsProvider>
-              <Button
-                className={classes.modalButton}
-                variant="contained"
-                color="primary"
-                onClick={handleAddTask}
-              >
-                Add Task
-              </Button>
-            </FormGroup>
-          </form>
+          <Form />
         </div>
       </Modal>
     </div>
