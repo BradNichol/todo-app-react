2c6264d120e986e57b477f79ece47d02fed8becc
Bradley Nichol <bradleynichol@live.co.uk>
2020-04-29 22:25:07 +0100

    Form component created

---
+++ src/AddModal/Form.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import FormGroup from "@material-ui/core/FormGroup";
+
+const Form = (props) => {
+  // state to control task input field
+  const [taskText, setTaskText] = useState("");
+  const handleTaskTextChange = (event) => setTaskText(event.target.value);
+
+  // state to control date picker change
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = (date) => setSelectedDate(date);
+
+  // state to control type select change
+  const [type, setType] = useState("");
+  const handleTypeChange = (event) => setType(event.target.value);
+
+  const handleAddTask = () => {
+    props.addNewTaskFunc(taskText, type, selectedDate);
+    setTaskText("");
+    setType();
+    setSelectedDate(new Date());
+    //setOpen(false);
+  };
+
+  return (
+    <form>
+        <FormGroup style={{padding:5}}>
+        <TextField
+            id="task"
+            label="Task"
+            value={taskText}
+            onChange={handleTaskTextChange}
+        />
+        <InputLabel id="task-type">Task Type</InputLabel>
+        <Select
+            labelId="task-type"
+            id="task-type-select"
+            value={type}
+            onChange={handleTypeChange}
+        >
+            <MenuItem value="work">Work</MenuItem>
+            <MenuItem value="personal">Personal</MenuItem>
+        </Select>
+
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+            value={selectedDate}
+            label="Due Date"
+            onChange={handleDateChange}
+            />
+        </MuiPickersUtilsProvider>
+        <Button
+            style={{marginTop: 40}}
+            variant="contained"
+            color="primary"
+            onClick={handleAddTask}
+        >
+            Add Task
+        </Button>
+        </FormGroup>
+  </form>
+  )
+};
+
+export default Form;
